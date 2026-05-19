3135c14459a8a39dd9dcae4de462a9a6ab5dbc59
zen-aperios <roberteaperios@gmail.com>
2026-05-19 09:41:50 +0100

    Set default book width scale to 3.2

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ const STARTUP_SETTINGS = {
   sizePattern: "uniform",
   loadAnimation: "dropFade",
   sceneTone: "darker",
-  widthScale: 3.0,
+  widthScale: 3.2,
   heightScale: 0.96,
   depthScale: 1.43,
   edgeRoundness: STARTUP_EDGE_ROUNDNESS,
